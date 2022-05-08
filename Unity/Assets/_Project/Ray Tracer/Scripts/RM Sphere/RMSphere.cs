978a102b813d83239043a348bf34c029053de8a4
abredenbals <anton9731@t-online.de>
2022-05-08 20:53:40 +0200

    added uml draft

---
+++ Unity/Assets/_Project/Ray Tracer/Scripts/RM Sphere/RMSphere.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace _Project.Ray_Tracer.Scripts.RM_Sphere
+{
+    public class RMSphere
+    {
+        public Vector3 Origin { get; set;}
+        
+        public float Radius { get; set;}
+        
+        public Color Color { get; set;}
+
+        public RMSphere()
+        {
+            Origin = Vector3.zero;
+            Radius = 0.0f;
+            Color = Color.black;
+        }
+
+        
+        public RMSphere(Vector3 origin, float radius, Color color)
+        {
+            Origin = origin;
+            Radius = radius;
+            Color = color;
+        }
+    }
+}
---
+++ Unity/Assets/_Project/Ray Tracer/Scripts/RM Sphere/SphereObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace _Project.Ray_Tracer.Scripts.RM_Sphere
+{
+    public class SphereObject : MonoBehaviour
+    {
+        private RMSphere sphere;
+
+        public RMSphere Sphere
+        {
+            get { return sphere; }
+            set { sphere = value; Reset(); }
+        }
+        
+        public float DrawRadius { get; private set; }
+    }
+}
